3db2d10d65a5fb6a5712e87cdc6b115e162406f5
chaman-rawat <nakulrawat2001@gmail.com>
2023-03-02 02:19:03 +0530

    feat: added dark theme removed sass variables with css variables for theming

--- components/NavBar/Navbar.js
+++ components/NavBar/Navbar.js
@@ -1,12 +1,15 @@
 import { FaRegMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa";
 import classes from "./Navbar.module.scss";
 
-export default function Navbar() {
+export default function Navbar(param) {
   return (
     <nav className={`container ${classes.navbar}`}>
       <h1>Where in the world?</h1>
-      <strong className={classes.theme__switcher}>
-        <FaRegMoon /> Dark Mode
+      <strong className={classes.theme__switcher} onClick={param.themeChange}>
+        {param.curTheme === "light" ? <FaRegMoon /> : <FaSun />}
+        {` ${param.curTheme === "light" ? "Light" : "Dark"} `}
+        Mode
       </strong>
     </nav>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,29 @@
 import "../styles/globals.scss";
+import { useState, useEffect } from "react";
 import Navbar from "../components/NavBar/Navbar";
 
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    const localState = JSON.parse(localStorage.getItem("savedTheme"));
+
+    if (theme === null && localState !== null) {
+      setTheme(localState);
+    } else {
+      localStorage.setItem("savedTheme", JSON.stringify(theme));
+    }
+  }, [theme]);
+
+  const onThemeChange = () => {
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
   return (
-    <>
-      <Navbar />
+    <main data-theme={theme}>
+      <Navbar themeChange={onThemeChange} curTheme={theme} />
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
 
